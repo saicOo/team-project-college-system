87bb8039b563b77be19889183e06f81464b209f8
unknown <sayed.ahmed.elbadry@gmail.com>
2022-02-19 14:30:38 -0800

    project laravel college system student

---
+++ app/Private_qa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Private_qa;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Private_qa extends Model 
+{
+
+    protected $table = 'private_qas';
+    public $timestamps = true;
+
+}
---
+++ app/Student_details.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Student_detais;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student_details extends Model 
+{
+
+    protected $table = 'student_detailss';
+    public $timestamps = true;
+
+    public function department()
+    {
+        return $this->belongsTo('Department', 'dept_id');
+    }
+
+}
